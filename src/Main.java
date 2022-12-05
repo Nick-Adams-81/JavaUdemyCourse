0101f82e94ffd1b311d53f21b4176d5a7bf266cf
Nick Adams <nick.pappas.adams@gmail.com>
2022-12-05 13:11:42 -0600

    added points total

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("NN       N        A");
+        System.out.println("N N      N       A A");
+        System.out.println("N   N    N      A   A");
+        System.out.println("N    N   N     A     A");
+        System.out.println("N     N  N    AAAAAAAAA");
+        System.out.println("N      N N   A         A");
+        System.out.println("N       NN  A           A");
     }
+
 }
---
+++ src/Module1/KeepScore.java
@@ -0,0 +1,16 @@
+package Module1;
+
+public class KeepScore {
+
+    public static void main(String[] args) {
+        int points = 0;
+        points -= 50;
+        points -= 3;
+        points += 30;
+        points += 100;
+        points += 60;
+        System.out.println(points);
+    }
+
+
+}
