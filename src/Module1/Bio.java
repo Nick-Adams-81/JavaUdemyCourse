5b6fb85db1b0ebd00ae27008cca987bc89cfa1e7
Nick Adams <nick.pappas.adams@gmail.com>
2022-12-05 13:19:27 -0600

    added string bio

---
+++ src/Module1/Bio.java
@@ -0,0 +1,17 @@
+package Module1;
+
+public class Bio {
+
+
+    public static void main(String[] args) {
+        String name = "Nick";
+        int age = 41;
+        String location = "Sacramento";
+        String sport = "Baseball";
+        int hoursPlayed = 2;
+        String game = "Chess";
+        String subject = "Math";
+        char grade = 'A';
+        System.out.println("My Name is " + name + ", I am " + age + " Years old, I'm From " + location + ", I love to play " + sport + ", i play for " + hoursPlayed + " hours a day. When i'm done i like to play " + game + ". I'm in school I study " + subject + ", I got an " + grade + ".");
+    }
+}
